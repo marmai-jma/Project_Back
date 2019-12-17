75852104ee9172492d35991f344259c0731eac26
marmai-jma <johane.marmai@gmail.com>
2019-12-17 20:10:33 +0100

    016 ajout addCorsMapping pour donner accès back au front

--- src/main/java/com/bnpparibas/bddf/projet/ProjetApplication.java
+++ src/main/java/com/bnpparibas/bddf/projet/ProjetApplication.java
@@ -2,13 +2,21 @@ package com.bnpparibas.bddf.projet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ProjetApplication {
+public class ProjetApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetApplication.class, args);
 		System.out.println("hello world");
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedMethods("GET", "PUT", "POST","DELETE");
 	}
 
 }
