1cb33b8b08a197f02e8aa2fc98e9811c52822d9d
marmai-jma <johane.marmai@gmail.com>
2019-12-27 16:24:05 +0100

    modification data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,7 +8,7 @@ INSERT INTO MEDIA (ID, LABEL, CATEGORY, TYPE, AUTHOR_NAME, AUTHOR_SURNAME, DESCR
 VALUES ('3', 'L''appel de Cthulhu','JEU', 'HISTORIQUE', 'Petersen','Sandy','Ce jeu emprunte à l’univers de H. P. Lovecraft son ambiance mystérieuse et oppressante. Les joueurs sont confrontés à des situations où ils doivent déjouer les complots de ces créatures et de leurs adorateurs. ','https://upload.wikimedia.org/wikipedia/en/3/33/Call_of_Cthulhu_RPG_1st_ed_1981.jpg','1981-01-01');
 
 INSERT INTO MEDIA (ID, LABEL, CATEGORY, TYPE, AUTHOR_NAME, AUTHOR_SURNAME, DESCRIPTION, MEDIA_IMAGE_URL, PUBLICATION_DATE)
-VALUES ('4', 'Le seigneur des anneaux','LIVRE', 'FANTASY', 'Tolkien','J.R.R','Roman en 3 prenant place dans le monde de fiction de la Terre du Milieu, il suit la quête du hobbit Frodo Bessac, qui doit détruire l''Anneau unique afin que celui-ci ne tombe pas entre les mains de Sauron, le Seigneur des ténèbres.','https://images.fr.shopping.rakuten.com/photo/1270487382.jpg','1954-07-29');
+VALUES ('4', 'Le seigneur des anneaux','LIVRE', 'FANTASY', 'Tolkien','J.R.R','Roman prenant place dans le monde de fiction de la Terre du Milieu, il suit la quête du hobbit Frodo Bessac, qui doit détruire l''Anneau unique afin que celui-ci ne tombe pas entre les mains de Sauron, le Seigneur des ténèbres.','https://images.fr.shopping.rakuten.com/photo/1270487382.jpg','1954-07-29');
 
 INSERT INTO MEDIA (ID, LABEL, CATEGORY, TYPE, AUTHOR_NAME, AUTHOR_SURNAME, DESCRIPTION, MEDIA_IMAGE_URL, PUBLICATION_DATE)
 VALUES ('5', 'Starwars 1', 'FILM', 'SF', 'Lucas','George', 'Il y a bien longtemps, dans une galaxie très lointaine... La guerre civile fait rage entre l''Empire galactique et l''Alliance rebelle.', 'https://images-na.ssl-images-amazon.com/images/I/61zAtWHeXLL._SY550_.jpg', '1977-10-19');
@@ -20,7 +20,7 @@ INSERT INTO MEDIA (ID, LABEL, CATEGORY, TYPE, AUTHOR_NAME, AUTHOR_SURNAME, DESCR
 VALUES ('7', 'L''appel de Cthulhu 1','JEU', 'HISTORIQUE', 'Petersen','Sandy','Ce jeu emprunte à l’univers de H. P. Lovecraft son ambiance mystérieuse et oppressante. Les joueurs sont confrontés à des situations où ils doivent déjouer les complots de ces créatures et de leurs adorateurs. ','https://upload.wikimedia.org/wikipedia/en/3/33/Call_of_Cthulhu_RPG_1st_ed_1981.jpg','1981-01-01');
 
 INSERT INTO MEDIA (ID, LABEL, CATEGORY, TYPE, AUTHOR_NAME, AUTHOR_SURNAME, DESCRIPTION, MEDIA_IMAGE_URL, PUBLICATION_DATE)
-VALUES ('8', 'Le seigneur des anneaux 1','LIVRE', 'FANTASY', 'Tolkien','J.R.R','Roman en 3 prenant place dans le monde de fiction de la Terre du Milieu, il suit la quête du hobbit Frodo Bessac, qui doit détruire l''Anneau unique afin que celui-ci ne tombe pas entre les mains de Sauron, le Seigneur des ténèbres.','https://images.fr.shopping.rakuten.com/photo/1270487382.jpg','1954-07-29');
+VALUES ('8', 'Le seigneur des anneaux 1','LIVRE', 'FANTASY', 'Tolkien','J.R.R','Roman prenant place dans le monde de fiction de la Terre du Milieu, il suit la quête du hobbit Frodo Bessac, qui doit détruire l''Anneau unique afin que celui-ci ne tombe pas entre les mains de Sauron, le Seigneur des ténèbres.','https://images.fr.shopping.rakuten.com/photo/1270487382.jpg','1954-07-29');
 
 INSERT INTO MEDIA (ID, LABEL, CATEGORY, TYPE, AUTHOR_NAME, AUTHOR_SURNAME, DESCRIPTION, MEDIA_IMAGE_URL, PUBLICATION_DATE)
 VALUES ('9', 'Starwars 2', 'FILM', 'SF', 'Lucas','George', 'Il y a bien longtemps, dans une galaxie très lointaine... La guerre civile fait rage entre l''Empire galactique et l''Alliance rebelle.', 'https://images-na.ssl-images-amazon.com/images/I/61zAtWHeXLL._SY550_.jpg', '1977-10-19');
@@ -32,7 +32,7 @@ INSERT INTO MEDIA (ID, LABEL, CATEGORY, TYPE, AUTHOR_NAME, AUTHOR_SURNAME, DESCR
 VALUES ('11', 'L''appel de Cthulhu 2','JEU', 'HISTORIQUE', 'Petersen','Sandy','Ce jeu emprunte à l’univers de H. P. Lovecraft son ambiance mystérieuse et oppressante. Les joueurs sont confrontés à des situations où ils doivent déjouer les complots de ces créatures et de leurs adorateurs. ','https://upload.wikimedia.org/wikipedia/en/3/33/Call_of_Cthulhu_RPG_1st_ed_1981.jpg','1981-01-01');
 
 INSERT INTO MEDIA (ID, LABEL, CATEGORY, TYPE, AUTHOR_NAME, AUTHOR_SURNAME, DESCRIPTION, MEDIA_IMAGE_URL, PUBLICATION_DATE)
-VALUES ('12', 'Le seigneur des anneaux 2','LIVRE', 'FANTASY', 'Tolkien','J.R.R','Roman en 3 prenant place dans le monde de fiction de la Terre du Milieu, il suit la quête du hobbit Frodo Bessac, qui doit détruire l''Anneau unique afin que celui-ci ne tombe pas entre les mains de Sauron, le Seigneur des ténèbres.','https://images.fr.shopping.rakuten.com/photo/1270487382.jpg','1954-07-29');
+VALUES ('12', 'Le seigneur des anneaux 2','LIVRE', 'FANTASY', 'Tolkien','J.R.R','Roman prenant place dans le monde de fiction de la Terre du Milieu, il suit la quête du hobbit Frodo Bessac, qui doit détruire l''Anneau unique afin que celui-ci ne tombe pas entre les mains de Sauron, le Seigneur des ténèbres.','https://images.fr.shopping.rakuten.com/photo/1270487382.jpg','1954-07-29');
 
 INSERT INTO MEDIA (ID, LABEL, CATEGORY, TYPE, AUTHOR_NAME, AUTHOR_SURNAME, DESCRIPTION, MEDIA_IMAGE_URL, PUBLICATION_DATE)
 VALUES ('13', 'Starwars 3', 'FILM', 'SF', 'Lucas','George', 'Il y a bien longtemps, dans une galaxie très lointaine... La guerre civile fait rage entre l''Empire galactique et l''Alliance rebelle.', 'https://images-na.ssl-images-amazon.com/images/I/61zAtWHeXLL._SY550_.jpg', '1977-10-19');
@@ -44,7 +44,7 @@ INSERT INTO MEDIA (ID, LABEL, CATEGORY, TYPE, AUTHOR_NAME, AUTHOR_SURNAME, DESCR
 VALUES ('15', 'L''appel de Cthulhu 3','JEU', 'HISTORIQUE', 'Petersen','Sandy','Ce jeu emprunte à l’univers de H. P. Lovecraft son ambiance mystérieuse et oppressante. Les joueurs sont confrontés à des situations où ils doivent déjouer les complots de ces créatures et de leurs adorateurs. ','https://upload.wikimedia.org/wikipedia/en/3/33/Call_of_Cthulhu_RPG_1st_ed_1981.jpg','1981-01-01');
 
 INSERT INTO MEDIA (ID, LABEL, CATEGORY, TYPE, AUTHOR_NAME, AUTHOR_SURNAME, DESCRIPTION, MEDIA_IMAGE_URL, PUBLICATION_DATE)
-VALUES ('16', 'Le seigneur des anneaux 3','LIVRE', 'FANTASY', 'Tolkien','J.R.R','Roman en 3 prenant place dans le monde de fiction de la Terre du Milieu, il suit la quête du hobbit Frodo Bessac, qui doit détruire l''Anneau unique afin que celui-ci ne tombe pas entre les mains de Sauron, le Seigneur des ténèbres.','https://images.fr.shopping.rakuten.com/photo/1270487382.jpg','1954-07-29');
+VALUES ('16', 'Le seigneur des anneaux 3','LIVRE', 'FANTASY', 'Tolkien','J.R.R','Roman prenant place dans le monde de fiction de la Terre du Milieu, il suit la quête du hobbit Frodo Bessac, qui doit détruire l''Anneau unique afin que celui-ci ne tombe pas entre les mains de Sauron, le Seigneur des ténèbres.','https://images.fr.shopping.rakuten.com/photo/1270487382.jpg','1954-07-29');
 
 INSERT INTO MEDIA (ID, LABEL, CATEGORY, TYPE, AUTHOR_NAME, AUTHOR_SURNAME, DESCRIPTION, MEDIA_IMAGE_URL, PUBLICATION_DATE)
 VALUES ('17', 'Starwars 7', 'FILM', 'SF', 'Lucas','George', 'Il y a bien longtemps, dans une galaxie très lointaine... La guerre civile fait rage entre l''Empire galactique et l''Alliance rebelle.', 'https://images-na.ssl-images-amazon.com/images/I/61zAtWHeXLL._SY550_.jpg', '1977-10-19');
@@ -56,7 +56,7 @@ INSERT INTO MEDIA (ID, LABEL, CATEGORY, TYPE, AUTHOR_NAME, AUTHOR_SURNAME, DESCR
 VALUES ('19', 'L''appel de Cthulhu 4','JEU', 'HISTORIQUE', 'Petersen','Sandy','Ce jeu emprunte à l’univers de H. P. Lovecraft son ambiance mystérieuse et oppressante. Les joueurs sont confrontés à des situations où ils doivent déjouer les complots de ces créatures et de leurs adorateurs. ','https://upload.wikimedia.org/wikipedia/en/3/33/Call_of_Cthulhu_RPG_1st_ed_1981.jpg','1981-01-01');
 
 INSERT INTO MEDIA (ID, LABEL, CATEGORY, TYPE, AUTHOR_NAME, AUTHOR_SURNAME, DESCRIPTION, MEDIA_IMAGE_URL, PUBLICATION_DATE)
-VALUES ('20', 'Bilbo le hobbit','LIVRE', 'FANTASY', 'Tolkien','J.R.R','Roman en 3 prenant place dans le monde de fiction de la Terre du Milieu, il suit la quête du hobbit Frodo Bessac, qui doit détruire l''Anneau unique afin que celui-ci ne tombe pas entre les mains de Sauron, le Seigneur des ténèbres.','https://images.fr.shopping.rakuten.com/photo/1270487382.jpg','1954-07-29');
+VALUES ('20', 'Bilbo le hobbit','LIVRE', 'FANTASY', 'Tolkien','J.R.R','Roman prenant place dans le monde de fiction de la Terre du Milieu, il suit la quête du hobbit Frodo Bessac, qui doit détruire l''Anneau unique afin que celui-ci ne tombe pas entre les mains de Sauron, le Seigneur des ténèbres.','https://images.fr.shopping.rakuten.com/photo/1270487382.jpg','1954-07-29');
 
 INSERT INTO MEDIA (ID, LABEL, CATEGORY, TYPE, AUTHOR_NAME, AUTHOR_SURNAME, DESCRIPTION, MEDIA_IMAGE_URL, PUBLICATION_DATE)
 VALUES ('21', 'Starwars 8', 'FILM', 'SF', 'Lucas','George', 'Il y a bien longtemps, dans une galaxie très lointaine... La guerre civile fait rage entre l''Empire galactique et l''Alliance rebelle.', 'https://images-na.ssl-images-amazon.com/images/I/61zAtWHeXLL._SY550_.jpg', '1977-10-19');
@@ -68,7 +68,7 @@ INSERT INTO MEDIA (ID, LABEL, CATEGORY, TYPE, AUTHOR_NAME, AUTHOR_SURNAME, DESCR
 VALUES ('23', 'L''appel de Cthulhu 5','JEU', 'HISTORIQUE', 'Petersen','Sandy','Ce jeu emprunte à l’univers de H. P. Lovecraft son ambiance mystérieuse et oppressante. Les joueurs sont confrontés à des situations où ils doivent déjouer les complots de ces créatures et de leurs adorateurs. ','https://upload.wikimedia.org/wikipedia/en/3/33/Call_of_Cthulhu_RPG_1st_ed_1981.jpg','1981-01-01');
 
 INSERT INTO MEDIA (ID, LABEL, CATEGORY, TYPE, AUTHOR_NAME, AUTHOR_SURNAME, DESCRIPTION, MEDIA_IMAGE_URL, PUBLICATION_DATE)
-VALUES ('24', 'Le Silmarillion','LIVRE', 'FANTASY', 'Tolkien','J.R.R','Roman en 3 prenant place dans le monde de fiction de la Terre du Milieu, il suit la quête du hobbit Frodo Bessac, qui doit détruire l''Anneau unique afin que celui-ci ne tombe pas entre les mains de Sauron, le Seigneur des ténèbres.','https://images.fr.shopping.rakuten.com/photo/1270487382.jpg','1954-07-29');
+VALUES ('24', 'Le Silmarillion','LIVRE', 'FANTASY', 'Tolkien','J.R.R','Roman prenant place dans le monde de fiction de la Terre du Milieu, il suit la quête du hobbit Frodo Bessac, qui doit détruire l''Anneau unique afin que celui-ci ne tombe pas entre les mains de Sauron, le Seigneur des ténèbres.','https://images.fr.shopping.rakuten.com/photo/1270487382.jpg','1954-07-29');
 
 INSERT INTO USER (ID, LOGIN, PASSWORD, USER_NAME, USER_SURNAME, AVATAR_IMAGE_URL, EMAIL, ACTIVE)
 VALUES ('99991','Bilbo123','','John','Doe', 'https://greendestinations.org/wp-content/uploads/2019/05/avatar-exemple-300x277.jpg','john.doe@gmail.com','TRUE');
